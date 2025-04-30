435e2f307a0928829c7d3cd949eca2bf40cc7316
user <user@DESKTOP-1VKM0JD>
2025-04-30 16:49:53 +0900

    ui수정

--- SeaScape/src/com/scape/activate/FrontController.java
+++ SeaScape/src/com/scape/activate/FrontController.java
@@ -2,35 +2,35 @@ package com.scape.activate;
 
 import java.util.Scanner;
 
-
 public class FrontController {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);				
+        boolean isStop = false;
+        ActivateControllerInterface controller = null;
 
-	public static void main(String[] args) {
-		//사용자가 누구인지 물어봄
-		Scanner sc = new Scanner(System.in);				
-		boolean isStop = false;
-		ActivateControllerInterface controller = null;
-		while(!isStop) {
-			System.out.println("뭐 여기 오신거 환영합니다.");
-			System.out.println("당신은 누구입니까? (1. 유저 2. 제작자 3. 매장 4. 본부)");
-			String job = sc.next();
-			switch(job) {
-			case "users"->{ controller = ControllerFactory.make("users");}
-			case "creator"->{ controller = ControllerFactory.make("creator");}
-			case "store"->{ controller = ControllerFactory.make("store");}
-			case "boss"->{ controller = ControllerFactory.make("boss");}
-			case "end"->{isStop=true;continue;}
-			default ->{continue;}
-			}
-			controller.execute();
-		}
-		sc.close();
-		System.out.println("다음에 또 오세요!");
-	}
+        while (!isStop) {
+        	System.out.println("+------------------------------------------+");
+        	System.out.println("|       🌊 Seascape에 오신 걸 환영합니다 🌊     |");
+        	System.out.println("+------------------------------------------+");
+        	System.out.println("   당신의 역할을 선택해주세요 👇");
+        	System.out.println("   1. 👤 users       2. 🛠️ creator");
+        	System.out.println("   3. 🏪 store       4. 🧑‍💼 boss");
+        	System.out.println("   종료하려면 'end'를 입력하세요.");
+        	System.out.print("\n👉 입력: ");
+
+            
+            String job = sc.next();
+            switch (job) {
+                case "users"   -> controller = ControllerFactory.make("users");
+                case "creator" -> controller = ControllerFactory.make("creator");
+                case "store"   -> controller = ControllerFactory.make("store");
+                case "boss"    -> controller = ControllerFactory.make("boss");
+                case "end"     -> { isStop = true; continue; }
+                default        -> { continue; }
+            }
+            controller.execute();
+        }
+        sc.close();
+        System.out.println("👋 다음에 또 오세요! Seascape와 함께했던 시간, 잊지 마세요 🌅");
+    }
 }
-
-
-
-
-
-
